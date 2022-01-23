6a4041b87b6fe237abec527d3b8578273fa5c861
visdauas <visdauas@gmail.com>
2022-01-23 13:59:28 +0100

    Add Web3Context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Web3ContextProvider } from '../context/Web3Context'
+import { ConstructorFragment } from 'ethers/lib/utils'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <Web3ContextProvider>
+      <Component {...pageProps} />
+  </Web3ContextProvider>
+  )
 }
 
 export default MyApp
