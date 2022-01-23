49a0003e664743a1e74914b85d52be0fc6cc6a0f
visdauas <visdauas@gmail.com>
2022-01-23 19:22:30 +0100

    Fix error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Web3ContextProvider from '../context/Web3Context'
-import { ConstructorFragment } from 'ethers/lib/utils'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
